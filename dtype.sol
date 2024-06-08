68ac717141c0a9a2a204c0917555b8a5bac52185
AnikethDeb <techrunstheworld@gmail.com>
2024-06-08 10:03:42 +0530

    feat:dtype

---
+++ dtype.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract hello{
+//bool
+bool public hey;
+bool public no = true;
+
+//UINT
+uint public  hiii=44;
+uint public hwi=443;
+uint public  hwwi=3;
+
+//int
+
+int public it =-2;
+//min and max
+
+int public intMin = type(int).min;
+int public intMax = type(int).max;
+
+//ARRAY  {fixed and dynamic}
+
+bytes1 public a = 0xb5;
+bytes1 public b=0x55;
+
+//Adress 
+}
